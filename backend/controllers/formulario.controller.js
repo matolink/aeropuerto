2dd8293db0a57a06bd1458e59f599b8512544c43
Matías Gutiérrez <ma.gutierrez.or@gmail.com>
2022-10-18 16:18:22 -0300

    formulario endpoint modified for use with frontend

--- backend/controllers/formulario.controller.js
+++ backend/controllers/formulario.controller.js
@@ -1,18 +1,33 @@
 import { prisma } from '../utils/prisma.js'
 
+// export const getFormulario = async (req, res) => {
+//   const paises = await prisma.pais.findMany()
+//   const aeropuertos = await Promise.all(
+//     paises.map(async (e) => {
+//       let inserts = await prisma.aeropuerto.findMany({
+//         where: {
+//           id_pais: e.id,
+//         },
+//       })
+//       return { [e.nombre]: inserts }
+//     })
+//   )
+//   return aeropuertos
+// }
+
 export const getFormulario = async (req, res) => {
-  const paises = await prisma.pais.findMany()
-  const aeropuertos = await Promise.all(
-    paises.map(async (e) => {
-      let inserts = await prisma.aeropuerto.findMany({
+  const aeropuertos = await prisma.aeropuerto.findMany()
+  const paises = await Promise.all(
+    aeropuertos.map(async (e) => {
+      let inserts = await prisma.pais.findUnique({
         where: {
-          id_pais: e.id,
+          id: e.id_pais,
         },
       })
-      return { [e.nombre]: inserts }
+      return `${e.nombre} - ${inserts.nombre}`
     })
   )
-  return aeropuertos
+  return paises
 }
 
 export const postFormulario = async (req, res) => {}
