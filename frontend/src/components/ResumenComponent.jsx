f7359c487327b5a549468de74a8ceab7fccd6eec
CiroDelgado1803 <cirodelgado1803@gmail.com>
2022-10-31 14:58:25 -0300

    "Actualizado el componente de Resumen, se completa el esqueleto"

--- frontend/src/components/ResumenComponent.jsx
+++ frontend/src/components/ResumenComponent.jsx
@@ -11,7 +11,7 @@ function ResumenComponent() {
 					<h1 className="">Resumen de tu Reserva</h1>
 				</div>
 			</div>
-			<div className="contenedorPrincipal d-flex justify-content-around">
+			<div className="contenedorPrincipal rounded d-flex justify-content-around">
 				{/* Contenedor de Detalles de los pasajes */}
 				<div className="card m-2">
 					<div className="card-title m-2">
@@ -42,37 +42,55 @@ function ResumenComponent() {
 					</div>
 				</div>
 				{/* Contenedor de Equipajes y Asientos */}
-				<div className="contenedorAsientosEquipajes">
-					<div className="card m-2">
+				<div className="contenedorAsientosEquipajes ">
+					<div className="card m-2 cardAsientos">
 						<div className="card-title m-2">
 							<h4>Asientos</h4>
 						</div>
-						<div className="card-body">
-							<p>Tipo de Pasaje:</p>
-							<div className="pasajeIda container m-2">
-								<p>
-									<strong> Pasaje Ida</strong>
-								</p>
-								<p>Origen: Santiago</p>
-								<p>Destino: La Serena</p>
-								<p>Fecha de Partida: 11-12-2022 14:50</p>
+						<div className="card">
+							<div className="d-flex justify-content-around mx-2">
+								<p>Pasajero -</p>
+								<p>- Santiago > La Serena -</p>
+								<p>- La Serena > Santiago - </p>
 							</div>
+							<div className="d-flex justify-content-around mx-2">
+								<p>Adulto 1 -</p>
+								<p>- Asiento Ida: -</p>
+								<p>- Asiento Vuelta: - </p>
+							</div>
+							<div className="d-flex justify-content-around mx-2">
+								<p>Adulto 2 -</p>
+								<p>- Asiento Ida: -</p>
+								<p>- Asiento Vuelta: - </p>
+							</div>
+							<button className="btn btn-primary">
+								<Link to="/asientos">Modificar Asientos</Link>
+							</button>
 						</div>
 					</div>
 					<div className="card m-2">
 						<div className="card-title m-2">
 							<h4>Equipaje</h4>
 						</div>
-						<div className="card-body">
-							<p>Tipo de Pasaje:</p>
-							<div className="pasajeIda container m-2">
-								<p>
-									<strong> Pasaje Ida</strong>
-								</p>
-								<p>Origen: Santiago</p>
-								<p>Destino: La Serena</p>
-								<p>Fecha de Partida: 11-12-2022 14:50</p>
+						<div className="card">
+							<div className="d-flex justify-content-around mx-2">
+								<p>Pasajero -</p>
+								<p>- Santiago > La Serena -</p>
+								<p>- La Serena > Santiago - </p>
 							</div>
+							<div className="d-flex justify-content-around mx-2">
+								<p>Adulto 1 -</p>
+								<p>- No Seleccionado -</p>
+								<p>- No Seleccionado - </p>
+							</div>
+							<div className="d-flex justify-content-around mx-2">
+								<p>Adulto 2 -</p>
+								<p>- No Seleccionado -</p>
+								<p>- No Seleccionado - </p>
+							</div>
+							<button className="btn btn-primary">
+								<Link to="/asientos">Modificar Asientos</Link>
+							</button>
 						</div>
 					</div>
 				</div>
@@ -81,18 +99,88 @@ function ResumenComponent() {
 					<div className="card-title m-2">
 						<h4>Otros Servicios</h4>
 					</div>
-					<div className="card-body">
-						<p>Tipo de Pasaje:</p>
-						<div className="pasajeIda container m-2">
-							<p>
-								<strong> Pasaje Ida</strong>
-							</p>
-							<p>Origen: Santiago</p>
-							<p>Destino: La Serena</p>
-							<p>Fecha de Partida: 11-12-2022 14:50</p>
+					<div className="card">
+						<div>
+							<h5 className="m-2">Sin Filas</h5>
+							<div className="pasajeIda card-body d-flex">
+								<div>
+									<p className="containerText">
+										Olvidate de las filas, embarca rapidamente.
+									</p>
+								</div>
+								<div class="form-check form-switch ms-2">
+									<input
+										class="form-check-input"
+										type="checkbox"
+										id="flexSwitchCheckDefault"
+									></input>
+								</div>
+							</div>
 						</div>
+						<div className="card-footer">Desde 10.000</div>
 					</div>
-					<button>
+					<div className="card">
+						<div>
+							<h5 className="m-2">Seguro de Viaje</h5>
+							<div className="pasajeIda card-body d-flex">
+								<div>
+									<p className="containerText">
+										Asistencia medica, Equipaje asegurado y Asistencia 24 Horas
+										del dia!
+									</p>
+								</div>
+								<div class="form-check form-switch ms-2">
+									<input
+										class="form-check-input"
+										type="checkbox"
+										id="flexSwitchCheckDefault"
+									></input>
+								</div>
+							</div>
+						</div>
+						<div className="card-footer">Desde 15.000</div>
+					</div>
+					<div className="card">
+						<div>
+							<h5 className="m-2">Necesidades especiales</h5>
+							<div className="pasajeIda card-body d-flex">
+								<div>
+									<p className="containerText">
+										Servicios especiales para hacer tu viaje mas agradable!
+									</p>
+								</div>
+								<div class="form-check form-switch ms-2">
+									<input
+										class="form-check-input"
+										type="checkbox"
+										id="flexSwitchCheckDefault"
+									></input>
+								</div>
+							</div>
+						</div>
+						<div className="card-footer">Desde 25.000</div>
+					</div>
+					<div className="card">
+						<div>
+							<h5 className="m-2">Mascota en Cabina</h5>
+							<div className="pasajeIda card-body d-flex">
+								<div>
+									<p className="containerText">
+										Viaje con tu mascota en cabina! Sujeto a condiciones.
+									</p>
+								</div>
+								<div class="form-check form-switch ms-2">
+									<input
+										class="form-check-input"
+										type="checkbox"
+										id="flexSwitchCheckDefault"
+									></input>
+								</div>
+							</div>
+						</div>
+						<div className="card-footer">Desde 35.000</div>
+					</div>
+					<button className="btn btn-primary">
 						<Link to="/formulario">Continuar</Link>
 					</button>
 				</div>
